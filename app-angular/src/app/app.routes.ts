0b86f0a853e49d2922830c2ba16018090a9bf5b3
Dylan Almaguer <dylanalmaguer4@gmail.com>
2025-10-21 08:59:43 -0600

    migracion a angular

--- app-angular/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-// src/app/app.routes.ts
-import { Routes } from '@angular/router';
-import { PerfilCard } from './perfil-card/perfil-card';
-
-export const routes: Routes = [
-  { path: '', component: PerfilCard }
-];
