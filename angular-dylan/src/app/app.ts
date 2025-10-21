691e7e39c12369081b12789f81327656341534c8
Dylan Almaguer <dylanalmaguer4@gmail.com>
2025-10-21 11:37:27 -0600

    proyecto de angular

---
+++ angular-dylan/src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'dylan-almaguer-root',
+  templateUrl: './dylan_almaguer.component.html',
+  // Usa styles.css global → no necesitas app.component.css
+  // Si tienes app.component.css, descomenta esta línea:
+  // styleUrls: ['./app.component.css']
+})
+export class App {
+  protected readonly title = signal('Dylan Almaguer');
+}
+export class AppComponent {
+  name = 'Dylan Damian Almaguer Acuña';
+  specialty = 'Desarrollo de Software';
+  interests = ['Cocinar', 'Escuchar música'];
+}
